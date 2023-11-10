9b6b66911da1b9865c79882c5283742ba12df8aa
Hyeri Jang <chjang5621@naver.com>
2023-11-10 11:11:39 +0900

    Initial commit

---
+++ src/main/java/com/hyerijang/dailypay/DailyPayApplication.java
@@ -0,0 +1,13 @@
+package com.hyerijang.dailypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyPayApplication.class, args);
+	}
+
+}
