65d63f27d38698060764db50508cf1a023322543
Hyeri Jang <chjang5621@naver.com>
2023-11-11 12:05:12 +0900

    refactor : RegisterRequest를 class에서 record로 변경

--- src/main/java/com/hyerijang/dailypay/member/dto/RegisterRequest.java
+++ src/main/java/com/hyerijang/dailypay/member/dto/RegisterRequest.java
@@ -2,18 +2,12 @@ package com.hyerijang.dailypay.member.dto;
 
 import com.hyerijang.dailypay.member.entity.User;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class RegisterRequest {
 
-    @NotBlank
-    private String account;
-
-    @NotBlank
-    private String password;
+public record RegisterRequest(
+    @NotBlank String account,
+    @NotBlank String password
+) {
 
     public User toEntity() {
         return User.builder()
