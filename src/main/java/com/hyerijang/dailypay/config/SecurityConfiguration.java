f9b39609d58bcf4e461c30cfbb04c07065288398
Hyeri Jang <chjang5621@naver.com>
2023-11-11 21:03:04 +0900

    feat : 유저 기능에 Spring Security 적용
    로그인, 로그아웃, access Token 재발급

---
+++ src/main/java/com/hyerijang/dailypay/config/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.hyerijang.dailypay.config;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**"};
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+            .csrf(AbstractHttpConfigurer::disable) //csrf 비활성화
+            .authorizeHttpRequests(req ->
+                req.requestMatchers(WHITE_LIST_URL).permitAll() //WHITE_LIST_URL에 대해서는 인증X, 인가X
+                    .anyRequest().authenticated() //나머지는 인증 필요
+            )
+            .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .logout(logout ->
+                logout.logoutUrl("/api/v1/auth/logout")
+                    .addLogoutHandler(logoutHandler)
+                    .logoutSuccessHandler(
+                        (request, response, authentication) -> SecurityContextHolder.clearContext())
+            )
+        ;
+
+        return httpSecurity.build();
+    }
+
+
+}
