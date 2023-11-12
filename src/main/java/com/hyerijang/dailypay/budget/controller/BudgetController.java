eea491df107d7eb3be89a88edf5ac3249f12228a
Hyeri Jang <chjang5621@naver.com>
2023-11-12 16:01:57 +0900

    feat : 카테고리 목록 API 구현

--- src/main/java/com/hyerijang/dailypay/budget/controller/BudgetController.java
+++ src/main/java/com/hyerijang/dailypay/budget/controller/BudgetController.java
@@ -2,10 +2,16 @@ package com.hyerijang.dailypay.budget.controller;
 
 import com.hyerijang.dailypay.budget.domain.Budget;
 import com.hyerijang.dailypay.budget.domain.Category;
+import com.hyerijang.dailypay.budget.dto.CategoryDto;
 import com.hyerijang.dailypay.budget.repository.BudgetRepository;
 import com.hyerijang.dailypay.budget.service.BudgetService;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +30,23 @@ public class BudgetController {
         budgetRepository.save(
             Budget.builder().budgetAmount(10000L).category(Category.SAVING).build());
     }
+
+    @GetMapping("/categories")
+    ResponseEntity<Result> getBudgetCategories() {
+        List<CategoryDto> categoryDtoList = budgetService.getCategories();
+        
+        return ResponseEntity.ok()
+            .body(Result.builder()
+                .count(categoryDtoList.size())
+                .data(categoryDtoList).build());
+    }
+
+    @Getter
+    @Builder
+    static class Result<T> {
+
+        private int count;
+        private T data; // 리스트의 값
+    }
+
 }
