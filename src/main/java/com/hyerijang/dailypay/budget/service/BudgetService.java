1634a8374c20296a5230ee51d27a8e09b98d0789
Hyeri Jang <chjang5621@naver.com>
2023-11-13 01:08:31 +0900

    feat : 예산 추천 기능 개발

--- src/main/java/com/hyerijang/dailypay/budget/service/BudgetService.java
+++ src/main/java/com/hyerijang/dailypay/budget/service/BudgetService.java
@@ -5,13 +5,16 @@ import com.hyerijang.dailypay.budget.domain.Category;
 import com.hyerijang.dailypay.budget.dto.BudgetDetail;
 import com.hyerijang.dailypay.budget.dto.CategoryDto;
 import com.hyerijang.dailypay.budget.dto.CreateBudgetListRequest;
+import com.hyerijang.dailypay.budget.dto.PlanBudgetRequest;
 import com.hyerijang.dailypay.budget.repository.BudgetRepository;
 import com.hyerijang.dailypay.common.exception.ApiException;
 import com.hyerijang.dailypay.common.exception.response.ExceptionEnum;
 import com.hyerijang.dailypay.user.domain.User;
 import com.hyerijang.dailypay.user.repository.UserRepository;
 import java.time.YearMonth;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -72,4 +75,38 @@ public class BudgetService {
             .user(user)
             .build();
     }
+
+    public List<BudgetDetail> getPlan(PlanBudgetRequest request) {
+
+        List<Object[]> result = budgetRepository.getTotalBudgetAmountByCategoryOrderBySumDesc();
+        long sumBudgetAmount = result.stream().mapToLong(r -> (long) r[1]).sum();
+        Map<Category, Integer> categoryRate = getCategoryRate(result, sumBudgetAmount);
+
+        categoryRate.forEach((category, rate) -> log.info("{} : {} ", category, rate));
+        return null;
+    }
+
+    private static Map<Category, Integer> getCategoryRate(List<Object[]> result,
+        long sumBudgetAmount) {
+        int miscellaneousPercentage = 100; //기타 비율
+        Map<Category, Integer> categoryRate = new HashMap<>();
+        for (Object[] r : result) {
+            int percentage = (int) (((Long) r[1] * 100.0) / sumBudgetAmount);
+            Category category = (Category) r[0];
+
+            if (category == Category.MISCELLANEOUS) {
+                continue;//기타
+            }
+
+            if (percentage >= 10) { // 전체 중 비중이 10% 미만이면 기타로 포함
+                miscellaneousPercentage -= percentage;
+                categoryRate.put(category, percentage);
+            }
+        }
+        categoryRate.put(Category.MISCELLANEOUS, miscellaneousPercentage); //기타 비율
+
+        return categoryRate;
+    }
+
+
 }
