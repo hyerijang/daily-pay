f5e39d7aa869b92bf14497b846abfe70f68d1367
Hyeri Jang <chjang5621@naver.com>
2023-11-13 01:08:31 +0900

    feat : 예산 추천 기능 개발

--- src/main/java/com/hyerijang/dailypay/budget/service/BudgetService.java
+++ src/main/java/com/hyerijang/dailypay/budget/service/BudgetService.java
@@ -5,15 +5,17 @@ import com.hyerijang.dailypay.budget.domain.Category;
 import com.hyerijang.dailypay.budget.dto.BudgetDetail;
 import com.hyerijang.dailypay.budget.dto.CategoryDto;
 import com.hyerijang.dailypay.budget.dto.CreateBudgetListRequest;
+import com.hyerijang.dailypay.budget.dto.PlanBudgetRequest;
 import com.hyerijang.dailypay.budget.repository.BudgetRepository;
 import com.hyerijang.dailypay.common.exception.ApiException;
 import com.hyerijang.dailypay.common.exception.response.ExceptionEnum;
 import com.hyerijang.dailypay.user.domain.User;
 import com.hyerijang.dailypay.user.repository.UserRepository;
 import java.time.YearMonth;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -54,7 +56,7 @@ public class BudgetService {
                     return savedBudget;
                 }
             )
-            .collect(Collectors.toList());
+            .toList();
 
         budgetRepository.saveAll(budgets);
         return BudgetDetail.getBudgetDetailList(budgets);
@@ -72,4 +74,40 @@ public class BudgetService {
             .user(user)
             .build();
     }
+
+    public List<BudgetDetail> recommendBudget(PlanBudgetRequest request) {
+
+        List<Object[]> result = budgetRepository.getUserBudgetTotalAmountByCategoryOrderBySumDesc();
+        long sumBudgetAmount = result.stream().mapToLong(r -> (long) r[1]).sum();
+        Map<Category, Integer> averageRatioByCategory = getAverageRatioByCategory(result,
+            sumBudgetAmount);
+
+        averageRatioByCategory.forEach(
+            (category, rate) -> log.info("{} : {} ", category, rate)); //로그
+
+        return BudgetDetail.generateBudgetDetails(request.userBudgetTotalAmount(),
+            averageRatioByCategory);
+    }
+
+    private static Map<Category, Integer> getAverageRatioByCategory(List<Object[]> result,
+        long sumBudgetAmount) {
+        int miscellaneousRatio = 100; //기타 비율
+        Map<Category, Integer> ratioByCategory = new HashMap<>();
+        for (Object[] r : result) {
+            int ratio = (int) (((Long) r[1] * 100.0) / sumBudgetAmount);
+            Category category = (Category) r[0];
+            if (category == Category.MISCELLANEOUS) {
+                continue;//기타
+            }
+            if (ratio >= 10) { // 전체 중 비중이 10% 미만이면 기타로 포함
+                miscellaneousRatio -= ratio;
+                ratioByCategory.put(category, ratio);
+            }
+        }
+        ratioByCategory.put(Category.MISCELLANEOUS, miscellaneousRatio); //기타 비율
+
+        return ratioByCategory;
+    }
+
+
 }
