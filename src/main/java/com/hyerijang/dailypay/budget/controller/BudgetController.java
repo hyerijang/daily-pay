93b90d9ae9031fedc09cdd6acd0c29fe0704a238
Hyeri Jang / 장혜리 <chjang5621@gmail.com>
2023-11-13 20:54:53 +0900

    Revert "refactor : 예산 추천 API 리펙토링"

--- src/main/java/com/hyerijang/dailypay/budget/controller/BudgetController.java
+++ src/main/java/com/hyerijang/dailypay/budget/controller/BudgetController.java
@@ -1,22 +1,18 @@
 package com.hyerijang.dailypay.budget.controller;
 
-import com.hyerijang.dailypay.budget.dto.BudgetDetail;
+import com.hyerijang.dailypay.budget.domain.Budget;
+import com.hyerijang.dailypay.budget.domain.Category;
 import com.hyerijang.dailypay.budget.dto.CategoryDto;
-import com.hyerijang.dailypay.budget.dto.CreateBudgetListRequest;
-import com.hyerijang.dailypay.budget.dto.PlanBudgetRequest;
 import com.hyerijang.dailypay.budget.repository.BudgetRepository;
 import com.hyerijang.dailypay.budget.service.BudgetService;
-import com.hyerijang.dailypay.common.aop.ExeTimer;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,11 +25,16 @@ public class BudgetController {
     private final BudgetService budgetService;
     private final BudgetRepository budgetRepository;
 
-    @ExeTimer
-    @GetMapping("/categories")
-    ResponseEntity<Result> saveBudgetList() {
-        List<CategoryDto> categoryDtoList = budgetService.getCategories();
+    @PostMapping
+    void test() {
+        budgetRepository.save(
+            Budget.builder().budgetAmount(10000L).category(Category.SAVING).build());
+    }
 
+    @GetMapping("/categories")
+    ResponseEntity<Result> getBudgetCategories() {
+        List<CategoryDto> categoryDtoList = budgetService.getCategories();
+        
         return ResponseEntity.ok()
             .body(Result.builder()
                 .count(categoryDtoList.size())
@@ -48,28 +49,4 @@ public class BudgetController {
         private T data; // 리스트의 값
     }
 
-    /**
-     * 예산 설정 및 업데이트 (금액만 변경 가능)
-     */
-
-    @ExeTimer
-    @PostMapping
-    ResponseEntity<Result> saveBudgetList(@RequestBody CreateBudgetListRequest request,
-        Authentication authentication) {
-        List<BudgetDetail> data = budgetService.createAll(request, authentication);
-        Result result = Result.builder().count(data.size()).data(data).build();
-        return ResponseEntity.ok().body(result);
-    }
-
-    /**
-     * 예산 설계(추천) API
-     */
-    @ExeTimer
-    @GetMapping
-    ResponseEntity<Result> planBudget(@RequestBody PlanBudgetRequest request,
-        Authentication authentication) {
-        List<BudgetDetail> data = budgetService.recommendBudget(request);
-        Result result = Result.builder().count(data.size()).data(data).build();
-        return ResponseEntity.ok().body(result);
-    }
 }
