4aa40968f1aa7e78fa17b0638aae5aff85e4848d
Hyeri Jang <chjang5621@naver.com>
2023-11-14 10:38:35 +0900

    feat : 새 지출 내역 (단건) 생성 API 구현

--- src/main/java/com/hyerijang/dailypay/expense/dto/ExpenseDto.java
+++ src/main/java/com/hyerijang/dailypay/expense/dto/ExpenseDto.java
@@ -1,8 +1,11 @@
 package com.hyerijang.dailypay.expense.dto;
 
 import com.hyerijang.dailypay.budget.domain.Category;
+import com.hyerijang.dailypay.expense.domain.Expense;
 import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record ExpenseDto(
     Long id,
     Long userId,
@@ -13,4 +16,15 @@ public record ExpenseDto(
     LocalDateTime expenseDate
 ) {
 
+    public static ExpenseDto of(Expense savedExpense) {
+        return ExpenseDto.builder()
+            .id(savedExpense.getId())
+            .userId(savedExpense.getUser().getId())
+            .category(savedExpense.getCategory())
+            .amount(savedExpense.getAmount())
+            .memo(savedExpense.getMemo())
+            .excludeFromTotal(savedExpense.isExcludeFromTotal())
+            .expenseDate(savedExpense.getExpenseDate())
+            .build();
+    }
 }
