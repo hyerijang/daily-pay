5ad6358c06f1a32f2d6533fd951e4cbae3e2078e
Hyeri Jang <chjang5621@naver.com>
2023-11-14 17:32:47 +0900

    test : 유저의 지출 내역(단건) 조회 API 테스트 추가

--- src/test/java/com/hyerijang/dailypay/expense/controller/ExpenseControllerTest.java
+++ src/test/java/com/hyerijang/dailypay/expense/controller/ExpenseControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -113,5 +114,55 @@ class ExpenseControllerTest {
             .andDo(print());
         // then
         verify(expenseService, times(1)).createExpense(any(), any());
+    }
+
+    @Test
+    @DisplayName("성공 : 지출 내역 조회 API 테스트 ")
+    void getAllExpenses() throws Exception {
+        // given
+        List<ExpenseDto> sampleExpenseDtoList = createSampleExpenseDtoList();
+
+        when(expenseService.getUserAllExpenses(any(), any()))
+            .thenReturn(sampleExpenseDtoList);
+
+        // when
+        mockMvc.perform(get("/api/v1/expenses")
+                .param("start", "2023-11-01")
+                .param("end", "2023-11-30")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.count").value(2))
+            .andExpect(jsonPath("$.data[0].amount").value(50000))
+            .andExpect(jsonPath("$.data[0].memo").value("Lunch"))
+            .andExpect(jsonPath("$.data[0].excludeFromTotal").value(false))
+            .andDo(print());
+        // then
+        verify(expenseService, times(1)).getUserAllExpenses(any(), any());
+
+    }
+
+    @Test
+    @DisplayName("성공 :  유저의 지출 내역(단건) 조회 API 테스트 ")
+    void getExpenseById() throws Exception {
+
+        when(expenseService.getExpenseById(any(), any()))
+            .thenReturn(createSampleExpenseDto());
+
+        //when
+        mockMvc.perform(get("/api/v1/expenses/1000")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.data.amount").value(50000))
+            .andExpect(jsonPath("$.data.memo").value("Lunch"))
+            .andExpect(jsonPath("$.data.excludeFromTotal").value(false))
+            .andExpect(
+                jsonPath("$.data.expenseDate").value("2023-11-14 12:30:00"))
+            .andDo(print());
+        //then
+
+        verify(expenseService, times(1)).getExpenseById(any(), any());
+
     }
 }
