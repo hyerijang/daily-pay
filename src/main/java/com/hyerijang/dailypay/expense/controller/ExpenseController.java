2c293a49c171951fd12fc6cc95ee604c07ad18f7
Hyeri Jang <chjang5621@naver.com>
2023-11-14 12:57:07 +0900

    feat : 유저의 지출 내역(단건) 수정 API

--- src/main/java/com/hyerijang/dailypay/expense/controller/ExpenseController.java
+++ src/main/java/com/hyerijang/dailypay/expense/controller/ExpenseController.java
@@ -18,9 +18,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -94,10 +94,11 @@ public class ExpenseController {
     /**
      * 유저의 지출 내역(단건) 수정 API
      */
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Result> updateExpense(@PathVariable Long id,
-        @RequestBody UpdateExpenseRequest updateExpenseRequest) {
-        ExpenseDto updatedExpenseDto = expenseService.updateExpense(id, updateExpenseRequest);
+        @RequestBody UpdateExpenseRequest updateExpenseRequest, Authentication authentication) {
+        ExpenseDto updatedExpenseDto = expenseService.updateExpense(id, updateExpenseRequest,
+            authentication);
         if (updatedExpenseDto != null) {
             return ResponseEntity.ok().body(Result.builder().data(updatedExpenseDto).build());
         }
