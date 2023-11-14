b7b53f45943b8be2afb3b3ce02a5e4cbf1351116
Hyeri Jang <chjang5621@naver.com>
2023-11-14 11:26:45 +0900

    refactor : 유저의 지출 내역(목록) 조회 시 기간 지정할 수 있게 수정

--- src/main/java/com/hyerijang/dailypay/expense/repository/ExpenseRepository.java
+++ src/main/java/com/hyerijang/dailypay/expense/repository/ExpenseRepository.java
@@ -2,6 +2,7 @@ package com.hyerijang.dailypay.expense.repository;
 
 import com.hyerijang.dailypay.expense.domain.Expense;
 import com.hyerijang.dailypay.user.domain.User;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
-    List<Expense> findAllByUser(User user);
+    List<Expense> findByExpenseDateBetweenAndUserAndDeletedIsFalse(LocalDateTime startDateTime,
+        LocalDateTime endDateTime, User user);
 }
