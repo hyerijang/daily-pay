9be248580ae3a82eaa3e08e140156789dd58a94a
Hyeri Jang <chjang5621@naver.com>
2023-11-16 18:42:45 +0900

    feat : 지출 통계 API 컨트롤러 구현

--- src/main/java/com/hyerijang/dailypay/statistics/controller/StatisticsController.java
+++ src/main/java/com/hyerijang/dailypay/statistics/controller/StatisticsController.java
@@ -4,13 +4,18 @@ import com.hyerijang.dailypay.common.aop.ExeTimer;
 import com.hyerijang.dailypay.common.exception.ApiException;
 import com.hyerijang.dailypay.common.exception.response.ExceptionEnum;
 import com.hyerijang.dailypay.statistics.service.StatisticsDummyDataGenerator;
+import com.hyerijang.dailypay.statistics.service.StatisticsService;
 import java.util.Arrays;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -19,11 +24,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/statistics")
 public class StatisticsController {
 
-    private final StatisticsDummyDataGenerator dummyDataGenerator;
-
     private final Environment environment;
+    private final StatisticsDummyDataGenerator dummyDataGenerator;
+    private final StatisticsService statisticsService;
 
-    // 개발 환경에서만 실행가능한 API, (application-dev.yml)
+    // === 통계  API === //
+
+    /**
+     * 지난 달 대비 총액 및 카테고리 별 소비율을 반환
+     */
+    public ResponseEntity<Result> getExpenseComparison(@RequestParam String condition,
+        Authentication authentication) {
+
+        Result result;
+
+        switch (condition) {
+            case "lastMonth":
+                // 지난 달 대비 총액 및 카테고리 별 소비율
+                result = Result.builder()
+                    .data(statisticsService.getExpenseComparisonLastMonth(authentication))
+                    .build();
+                break;
+            case "lastWeek":
+                // 지난주 같은 요일 대비 소비율
+//                result = Result.builder().data(statisticsService.getLastWeekSameDayComparison(authentication)).build();
+                break;
+            case "otherUser":
+                // 다른 유저 대비 소비율
+//                result = Result.builder().data(statisticsService.getExpenseComparisonWithOtherUser(authentication)).build();
+                break;
+            default:
+                // 다른 경우에 대한 처리 (예: 유효하지 않은 조건)
+                throw new ApiException(ExceptionEnum.WRONG_EXPENSE_COMPARISON_CONDITION);
+        }
+        return ResponseEntity.ok().body(result);
+    }
+
+
+    @Getter
+    @Builder
+    static class Result<T> {
+
+        private int count;
+        private T data; // 리스트의 값
+    }
+
+    // == 더미데이터 생성 == //
+
+    /**
+     * 개발 환경에서만 실행가능한 API, (application-dev.yml)
+     */
     @ExeTimer
     @PostMapping("/dummy-data")
     void generateDummy(Authentication authentication) {
