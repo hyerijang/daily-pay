b08cd7948a456cedf6edc21bcb7810d35b8f7283
Hyeri Jang <chjang5621@naver.com>
2023-11-16 12:02:40 +0900

    chore : 불필요한 Lombok 어노테이션 제거

--- src/main/java/com/hyerijang/dailypay/token/Token.java
+++ src/main/java/com/hyerijang/dailypay/token/Token.java
@@ -6,15 +6,18 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -30,8 +33,9 @@ public class Token {
 
     public TokenType tokenType = TokenType.BEARER;
 
+    @Setter
     public boolean revoked = true;
-
+    @Setter
     public boolean expired = true;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
