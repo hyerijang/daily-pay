0869f1b6699523b95cb0a46a883ada4711baf255
Hyeri Jang <chjang5621@naver.com>
2023-11-22 18:46:56 +0900

    refactor : expense 에 검증 어노테이션 추가

--- src/main/java/com/hyerijang/dailypay/expense/dto/UpdateExpenseRequest.java
+++ src/main/java/com/hyerijang/dailypay/expense/dto/UpdateExpenseRequest.java
@@ -4,23 +4,31 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hyerijang.dailypay.budget.domain.Category;
 import com.hyerijang.dailypay.expense.domain.Expense;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 @Schema(description = "지출 업데이트 요청")
 public record UpdateExpenseRequest
     (
+        @NotNull
         @Schema(description = "카테고리")
         Category category,
 
+        @Positive
         @Schema(description = "지출 금액")
         Long amount,
 
+        @NotNull
         @Schema(description = "메모")
         String memo,
 
+        @NotNull
         @Schema(description = "전체 지출 제외 유무")
         Boolean excludeFromTotal,
 
+        @PastOrPresent
         @Schema(description = "지출일시")
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime expenseDate
     ) {
