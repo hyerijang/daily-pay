b733f2ad77c64e58a3744d8f9844d67088c77575
Hyeri Jang <chjang5621@naver.com>
2023-11-22 18:40:13 +0900

    refactor : auth 에 검증 어노테이션 추가

--- src/main/java/com/hyerijang/dailypay/auth/AuthenticationRequest.java
+++ src/main/java/com/hyerijang/dailypay/auth/AuthenticationRequest.java
@@ -1,12 +1,17 @@
 package com.hyerijang.dailypay.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "인증(로그인) 요청")
 public record AuthenticationRequest(
     @Schema(description = "이메일")
+    @NotBlank
+    @Email
     String email,
     @Schema(description = "비밀번호")
+    @NotBlank
     String password) {
 
 }
