ea9cec333caefe3f40487cbe087e7c90a3cebd7c
Hyeri Jang <chjang5621@naver.com>
2023-11-23 14:29:12 +0900

    feat : CustomUserDetail 적용

---
+++ src/main/java/com/hyerijang/dailypay/auth/dto/CustomUserDetails.java
@@ -0,0 +1,71 @@
+package com.hyerijang.dailypay.auth.dto;
+
+
+import com.hyerijang.dailypay.user.domain.User;
+import java.util.Collection;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+
+    /* 유저의 권한 목록, 권한 반환*/
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null; // 아직 인가 구현 안함
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    /* 계정 만료 여부
+     * true :  만료 안됨
+     * false : 만료
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /* 계정 잠김 여부
+     * true : 잠기지 않음
+     * false : 잠김
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /* 비밀번호 만료 여부
+     * true : 만료 안 됨
+     * false : 만료
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /* 사용자 활성화 여부
+     * true : 활성화 됨
+     * false : 활성화 안 됨
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
