84ce58d3e008d4d7ff23b51f655c0a0b36b0e5a0
Hyeri Jang <chjang5621@naver.com>
2023-11-24 17:49:39 +0900

    fix : 검색 조건에서 시작, 종료일 null이어도 되게 수정
    #62 #65

--- src/main/java/com/hyerijang/dailypay/expense/dto/ExpenseSearchCondition.java
+++ src/main/java/com/hyerijang/dailypay/expense/dto/ExpenseSearchCondition.java
@@ -13,9 +13,17 @@ public record ExpenseSearchCondition(
 ) {
 
     public static ExpenseSearchCondition of(GetAllExpenseParam getAllExpenseParam, Long userId) {
+        //시작일의 0시 0분 0초
+        LocalDateTime start =
+            getAllExpenseParam.start() != null ? getAllExpenseParam.start().atStartOfDay() : null;
+
+        //종료일의 23시 59분 59초
+        LocalDateTime end =
+            getAllExpenseParam.end() != null ? getAllExpenseParam.end().atTime(23, 59, 59) : null;
+
         return ExpenseSearchCondition.builder()
-            .start(getAllExpenseParam.start().atStartOfDay()) //시작일으리 0시 0분 0초
-            .end(getAllExpenseParam.end().atTime(23, 59, 59)) //종료일의 23시 59분 59초
+            .start(start)
+            .end(end)
             .userId(userId)
             .category(getAllExpenseParam.category())
             .build();
