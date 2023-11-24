554be13a9e9a68c045187deffe68a76bfd1b1d21
Hyeri Jang <chjang5621@naver.com>
2023-11-25 00:29:59 +0900

    fix : v1 이 아닌 다른 버전의 url도 swagger에 포함되도록 수정
    #75

--- src/main/java/com/hyerijang/dailypay/config/SwaggerOpenAPIConfig.java
+++ src/main/java/com/hyerijang/dailypay/config/SwaggerOpenAPIConfig.java
@@ -34,7 +34,7 @@ public class SwaggerOpenAPIConfig {
     public GroupedOpenApi group1Config() {
         return GroupedOpenApi.builder()
             .group("v1-definition")
-            .pathsToMatch("/api/**")
+            .pathsToMatch("/api/**") //
             .build();
     }
 
