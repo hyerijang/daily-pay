57b6c6084f61d0729d3be7b3c9f9c5e6d7257a6c
Hyeri Jang <chjang5621@naver.com>
2023-12-19 20:29:54 +0900

    feat : @WithMockCurrentUser 생성

--- src/main/java/com/hyerijang/dailypay/auth/dto/UserAdapter.java
+++ src/main/java/com/hyerijang/dailypay/auth/dto/UserAdapter.java
@@ -4,6 +4,10 @@ package com.hyerijang.dailypay.auth.dto;
 import com.hyerijang.dailypay.user.domain.User;
 import lombok.Getter;
 
+/**
+ * CurrentUser 어노테이션으로 User 정보를 가져오기 위한 Adapter
+ * @see com.hyerijang.dailypay.auth.CurrentUser
+ */
 @Getter
 public class UserAdapter extends CustomUserDetails {
 
