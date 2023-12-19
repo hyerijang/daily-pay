5ef2d9b1e6f8829f89fe69a6e8a4cdfc708623ae
Hyeri Jang <chjang5621@naver.com>
2023-12-19 22:12:46 +0900

    test : BudgetController 테스트 수정

--- src/test/java/com/hyerijang/dailypay/budget/controller/BudgetControllerTest.java
+++ src/test/java/com/hyerijang/dailypay/budget/controller/BudgetControllerTest.java
@@ -6,106 +6,66 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyerijang.dailypay.WithMockCurrentUser;
 import com.hyerijang.dailypay.budget.domain.Category;
 import com.hyerijang.dailypay.budget.dto.BudgetResponse;
 import com.hyerijang.dailypay.budget.dto.CategoryResponse;
 import com.hyerijang.dailypay.budget.dto.CreateBudgetListRequest;
 import com.hyerijang.dailypay.budget.dto.CreateBudgetListRequest.CreateBudgetDetail;
+import com.hyerijang.dailypay.budget.dto.RecommendBudgetRequest;
 import com.hyerijang.dailypay.budget.repository.BudgetRepository;
 import com.hyerijang.dailypay.budget.service.BudgetService;
 import com.hyerijang.dailypay.config.JwtAuthenticationFilter;
-import com.hyerijang.dailypay.user.domain.User;
-import com.hyerijang.dailypay.user.repository.UserRepository;
+import com.hyerijang.dailypay.config.SecurityConfiguration;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-
+@Slf4j
 @DisplayName("단위테스트 - BudgetController")
-@WebMvcTest(controllers = BudgetController.class)
-@ExtendWith(SpringExtension.class)
+@WithMockCurrentUser
+@WebMvcTest(
+    value = {BudgetController.class}, // 특정 controller만 로딩하여 테스트
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+            classes = {SecurityConfiguration.class, JwtAuthenticationFilter.class})
+        //Spring, jwt 필터 제외
+    }
+)
 @AutoConfigureMockMvc(addFilters = false)
-public class BudgetControllerTest {
+class BudgetControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
 
     // === DI === //
     @MockBean
-    private UserRepository userRepository;
-
-    @MockBean
     private BudgetService budgetService;
-
     @MockBean
     private BudgetRepository budgetRepository;
 
-    @MockBean
-    private JwtAuthenticationFilter filter;
-
-
-
-    // === Request 초기화 === //
-
-    User user;
-    CreateBudgetListRequest createBudgetListRequest;
-
-    // === Response 초기화 === //
-
-    BudgetResponse budgetResponse = BudgetResponse.builder().build();
-    List<CategoryResponse> categoryResponseList = Category
-        .toList()
-        .stream()
-        .map((c) -> new CategoryResponse(c.getCode(), c.getTitle()))
-        .toList();
-
-    List<BudgetResponse> updatedBudgetResponseList = new ArrayList<>() {{
-        add(budgetResponse);
-    }};
-
-    @BeforeEach
-    void setUp() throws Exception {
-        //=== user ===//
-        user = User.builder().email("test@email").password("password").build();
-        ReflectionTestUtils.setField(user, "id", 11111L);
-        when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
-
-        // == Requset == //
-        List<CreateBudgetDetail> budgetDetails = new ArrayList<>() {{
-            add(new CreateBudgetDetail(Category.FOOD, 300000L));
-            add(new CreateBudgetDetail(Category.HOUSING, 500000L));
-        }};
-        createBudgetListRequest = CreateBudgetListRequest.builder()
-            .data(budgetDetails)
-            .yearMonth(YearMonth.now())
-            .build();
-    }
-
-    // === Test ===//
-    @WithMockUser
+    // === 카테고리 API  ===//
     @DisplayName("카테고리 API는 OK를 리턴한다")
     @Test
     void getCategories() throws Exception {
@@ -125,8 +85,38 @@ public class BudgetControllerTest {
             .andDo(print());
     }
 
-    @WithMockUser
-    @DisplayName("예산 설정 API는 requset body 가 null일 시 400을 리턴한다")
+    // === 예산 변경 API === //
+
+    // == Request == //
+    List<CreateBudgetDetail> budgetDetails = new ArrayList<>() {{
+        add(new CreateBudgetDetail(Category.FOOD, 300000L));
+        add(new CreateBudgetDetail(Category.HOUSING, 500000L));
+    }};
+
+    CreateBudgetListRequest createBudgetListRequest = CreateBudgetListRequest.builder()
+        .data(budgetDetails)
+        .yearMonth(YearMonth.now())
+        .build();
+
+    // == Response == //
+    List<CategoryResponse> categoryResponseList = Category
+        .toList()
+        .stream()
+        .map((c) -> new CategoryResponse(c.getCode(), c.getTitle()))
+        .toList();
+    BudgetResponse budgetResponse = BudgetResponse.builder().category(Category.FOOD).amount(300000L)
+        .build();
+    BudgetResponse budgetResponse2 = BudgetResponse.builder().category(Category.HOUSING)
+        .amount(500000L).build();
+
+
+    List<BudgetResponse> updatedBudgetResponseList = new ArrayList<>() {{
+        add(budgetResponse);
+        add(budgetResponse2);
+    }};
+
+
+    @DisplayName("예산 변경 API는 requset body가 null일 시 400을 리턴한다")
     @Test
     void updateBudgets_will_return400() throws Exception {
         // when
@@ -139,86 +129,86 @@ public class BudgetControllerTest {
     }
 
 
-    @WithMockUser
-    @DisplayName("--------------------------------")
+    @DisplayName("예산 변경 API는 성공시 ok를 리턴한다.")
     @Test
-    void updateBudgets() throws Exception {
+    void updateBudgets_will_return_ok() throws Exception {
+        //given
+        given(budgetService.update(any(), any())).willReturn(updatedBudgetResponseList);
+
         // when
-        ResultActions response = mockMvc.perform(post("/api/v1/budgets"));
+        ResultActions response = mockMvc.perform(post("/api/v1/budgets")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(createBudgetListRequest)
+            )
+        );
 
         // than
-        response.andExpect(status().is4xxClientError())
+        response.andExpect(status().isOk())
+            .andDo(print());
+    }
+
+    @DisplayName("예산 변경 API는 성공시 body에 변경 결과를 리턴한다.")
+    @Test
+    void updateBudgets() throws Exception {
+        //given
+        given(budgetService.update(any(), any())).willReturn(updatedBudgetResponseList);
+
+        // when
+        ResultActions response = mockMvc.perform(post("/api/v1/budgets")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(createBudgetListRequest)
+            )
+        );
+
+        // than
+        response.andExpect(status().isOk())
+            .andExpect(jsonPath("$.count").value(updatedBudgetResponseList.size()))
             .andDo(print());
     }
 
 
-//
-//    @DisplayName("예산 수정 API 테스트")
-//    @Test
-//    void testUpdateBudgets() throws Exception {
-//
-//        // given
-//        when(budgetService.update(any(CreateBudgetListRequest.class), any()))
-//            .thenReturn(budgetResponseList);
-//
-//        String requestBody = """
-//            {
-//              "data": [
-//                {
-//                  "category": "FOOD",
-//                  "amount": 50000
-//                },
-//                    {
-//                  "category": "UTILITIES",
-//                  "amount": 45000
-//                },
-//                {
-//                  "category": "MEDICAL",
-//                  "amount": 5000
-//                }
-//              ],
-//              "yearMonth": "2023-11"
-//            }
-//            """;
-//
-//        // when
-//        mockMvc.perform(post("/api/v1/budgets")
-//                .content(requestBody)
-//                .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk())
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//            .andDo(print());
-//
-//        // than
-//        verify(budgetService, times(1)).update(any(CreateBudgetListRequest.class),
-//            any());
-//
-//    }
-//
-//    @DisplayName("예산 추천 API 테스트")
-//    @Test
-//    void testRecommendBudgets() throws Exception {
-//
-//        // given
-//        when(budgetService.recommend(any(RecommendBudgetRequest.class))).thenReturn(
-//            budgetResponseList);
-//
-//        String requestBody = """
-//                {
-//                    "userBudgetTotalAmount" : 10000
-//                }
-//            """;
-//
-//        // when
-//        mockMvc.perform(get("/api/v1/budgets")
-//                .content(requestBody)
-//                .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk())
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//            .andDo(print());
-//
-//        // than
-//        verify(budgetService, times(1)).recommend(any(RecommendBudgetRequest.class));
-//
-//    }
+    // === 예산 추천 API  === //
+    @DisplayName("예산 추천 API는 requset body가 null일 시 400을 리턴한다")
+    void recommendBudgets_with_null_request() throws Exception {
+
+        // given
+        when(budgetService.recommend(any(RecommendBudgetRequest.class))).thenReturn(
+            updatedBudgetResponseList);
+
+        // when
+        ResultActions perform = mockMvc.perform(get("/api/v1/budgets")
+            .contentType(MediaType.APPLICATION_JSON));
+
+        // than
+        perform.andExpect(status().is4xxClientError())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andDo(print());
+
+    }
+
+    @DisplayName("예산 추천 API 테스트")
+    @Test
+    void recommendBudgets() throws Exception {
+
+        // given
+        when(budgetService.recommend(any(RecommendBudgetRequest.class))).thenReturn(
+            updatedBudgetResponseList);
+
+        String requestBody = """
+                {
+                    "userBudgetTotalAmount" : 10000
+                }
+            """;
+
+        // when
+        ResultActions perform = mockMvc.perform(get("/api/v1/budgets")
+            .content(requestBody)
+            .contentType(MediaType.APPLICATION_JSON));
+
+        // than
+        perform.andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andDo(print());
+
+    }
 }
