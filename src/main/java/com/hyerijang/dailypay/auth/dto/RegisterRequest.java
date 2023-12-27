ac1cedefcb9907397cb995302a3b57ee06e1664d
Hyeri Jang <chjang5621@naver.com>
2023-12-27 19:41:23 +0900

    refactor : 이메일, 비밀번호 검증 조건 추가

--- src/main/java/com/hyerijang/dailypay/auth/dto/RegisterRequest.java
+++ src/main/java/com/hyerijang/dailypay/auth/dto/RegisterRequest.java
@@ -4,15 +4,21 @@ import com.hyerijang.dailypay.user.domain.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 
 @Schema(description = "회원 가입 요청")
 public record RegisterRequest(
     @Schema(description = "이메일")
-    @Email
-    @NotBlank
+    @NotNull(message = "{email.not_null}")
+    @NotBlank(message = "{email.not_empty}")
+    @Email(message = "{email.not_email}")
     String email,
-    @NotBlank
+
+    @NotNull(message = "{password.not_null}")
+    @NotBlank(message = "{password.not_empty}")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{8,16}$", message = "{password.policy_violation}")
     String password
 ) {
 
