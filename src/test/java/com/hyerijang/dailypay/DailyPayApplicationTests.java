5802ec8fb8d2b1ef1021f81a878084e37298901d
Hyeri Jang <chjang5621@naver.com>
2024-01-01 23:54:13 +0900

    test3

--- src/test/java/com/hyerijang/dailypay/DailyPayApplicationTests.java
+++ src/test/java/com/hyerijang/dailypay/DailyPayApplicationTests.java
@@ -1,12 +1,10 @@
 package com.hyerijang.dailypay;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DailyPayApplicationTests {
 
-	// test2
 	@Test
 	void contextLoads() {
 	}
